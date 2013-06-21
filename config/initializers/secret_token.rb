488fb9fdfe68c0b1c8a9db84e8521d7c574e1aee
John Wayne <shermandos@gmail.com>
2013-06-20 19:49:55 -0700

    Fuck Yeah INITIAL COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JointApp::Application.config.secret_token = 'f36a1abcecd9557534a0f7342da43c5962d02963888927a1094593d4e64e22f9a4a8a556dbe0fc021713b7e033a9a13bacd8c6f7029a8662c083c9d497898219'
